128d096fae377abacc7e6750f2d56f086570d3bf
cphelps76 <cphelps76@gmail.com>
2013-07-21 21:31:34 -0500

    refine kernel custom toolchain definition for linaro builds

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -134,7 +134,7 @@ ifeq ($(TARGET_ARCH),arm)
         endif
       endif
     endif
-    ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),linaro)
+    ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),Linaro-4.7)
         ifeq ($(HOST_OS),darwin)
             ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
             include $(BUILD_SYSTEM)/linaro_compilerchecks.mk
